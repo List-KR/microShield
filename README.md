63e9200ad9b31eb5118500830038b4d0d22dc82b
PiQuark6046 <piquark6046@proton.me>
2022-05-07 10:04:33 +0000

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+## microShield
+microShield blocks Ad-Shield advertisement and anti-adblock script.
+
+### Quick Start
+Just click the following URL to detect this userscript.
+
+https://github.com/List-KR/microShield/raw/main/microShield.user.js
+
+### How to install
+- [Tampermonkey](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/) - Browser extension
+    1. Open settings of Tampermonkey.
+    2. Go to `Utilities` tab.
+    3. Input the following URL into `Install from URL`:
+        ```
+        https://github.com/List-KR/microShield/raw/main/microShield.user.js
+        ```
+    4. Confirm metadata of the userscript and click `Install`.
+    
+- AdGuard for Windows
+    1. Click `Settings`.
+    2. Click `Extensions`.
+    3. Click `Add extension`.
+    4. Input the following URL:
+        ```
+        https://github.com/List-KR/microShield/raw/main/microShield.user.js
+        ```
+    5. Click `Install`.
+    6. Confirm metadata of the userscript and click `Install`.
+
+
+- AdGuard for Android
+    1. Go to Settings -> Extension.
+    2. Touch `New extension`.
+    3. Input the following URL:
+        ```
+        https://github.com/List-KR/microShield/raw/main/microShield.user.js
+        ```
+    4. Touch `Next`.
+    5. Confirm metadata of the userscript and touch `Add`.
+
+
+ - AdGuard for iOS
+
+    Userscript is not supported currently on AdGuard for iOS.
+    However, It will be supported in the future.[^1]
+
+
+[^1]: https://github.com/AdguardTeam/AdguardForiOS/issues/1542
+
+### Why List-KR created microShield?
+Unlike license of List-KR Script, microShield is under MIT license that allows other software to use this userscript without using the same license.
+
+So, the adblock community can resist against Ad-Shield widely.
---
+++ microShield.user.js
@@ -0,0 +1,51 @@
+// ==UserScript==
+// @name         microShield
+// @encoding     utf-8
+// @namespace    https://github.com/List-KR/microShield
+// @homepageURL  https://github.com/List-KR/microShield
+// @supportURL   https://github.com/List-KR/microShield/issues
+// @updateURL    https://github.com/List-KR/microShield/raw/main/microShield.user.js
+// @downloadURL  https://github.com/List-KR/microShield/raw/main/microShield.user.js
+// @license      MIT
+//
+// @version      0.0.1
+// @author       PiQuark6046 and contributors
+//
+// @match        *://ad-shield.team/*
+// @match        *://algumon.com/*
+// @match        *://*.algumon.com/*
+// @match        *://loawa.com/*
+// @match        *://*.loawa.com/*
+// @match        *://*.inven.co.kr/*
+// @match        *://ygosu.com/*
+// @match        *://ppss.kr/*
+// @match        *://ad-shield.io/*
+// @match        *://sports.donga.com/*
+// @match        *://mlbpark.donga.com/*
+// @match        *://etoland.co.kr/*
+// @match        *://*.etoland.co.kr/*
+// @match        *://*.op.gg/*
+// @match        *://tgd.kr/*
+// @match        *://feedclick.net/*
+// @match        *://*.reddit.com/*
+// @match        *://*.nytimes.com/*
+// @match        *://*.forbes.com/*
+// @exclude      *://myaccount.nytimes.com/*
+// @exclude      *://tgd.kr/member/login_page*
+// @exclude      *://member.op.gg/*
+// @exclude      *://etoland.co.kr/bbs/register.php
+// @exclude      *://etoland.co.kr/bbs/password_lost.php
+// @exclude      *://secure.donga.com/*
+// @exclude      *://member.inven.co.kr/*
+//
+// @description        microShield allows the adblock community to resist against Ad-Shield widely.
+//
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    
+})();
