5d0ec2033fa45241f844365e41e9b2167ad6f5c8
PiQuark6046 <piquark6046@proton.me>
2022-06-12 04:33:48 +0000

    Update version

--- microShield.user.js
+++ microShield.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://github.com/List-KR/microShield/raw/main/microShield.user.js
 // @license      MIT
 //
-// @version      1.0
+// @version      1.0.1
 // @author       PiQuark6046 and contributors
 //
 // @match        *://ad-shield.team/*
