6536190614f51fe452f98ede3e451989b7a17ac2
PiQuark6046 <piquark6046@proton.me>
2023-08-20 04:41:15 +0000

    feat: add hometownstation.com/ as a website protected by Ad-Shield

--- microShield.user.js
+++ microShield.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://cdn.jsdelivr.net/gh/List-KR/microShield@main/microShield.user.js
 // @license      MIT
 //
-// @version      3.6.4
+// @version      3.6.5
 // @author       HoJeong Go and contributors
 //
 // @match        *://ad-shield.team/*
@@ -36,6 +36,8 @@
 // @match        *://*.hankyung.com/*
 // @match        *://isplus.com/*
 // @match        *://*.isplus.com/*
+// @match        *://hometownstation.com/*
+// @match        *://*.hometownstation.com/*
 //
 // @description        microShield allows the adblock community to resist against Ad-Shield widely.
 // @description:ko     microShield는 애드블록 커뮤니티가 애드쉴드에 널리 저항할 수 있도록 합니다.
