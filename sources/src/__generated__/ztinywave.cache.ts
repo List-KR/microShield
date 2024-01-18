cde11f39b348b864b90ddf22fe41d0329a83f6f9
piquark6046 <j.w.so@adguard.com>
2024-01-18 08:23:20 +0000

    feat: add some decryption keys for ztinywave
    https://github.com/AdguardTeam/AdguardFilters/issues/170923

--- sources/src/__generated__/ztinywave.cache.ts
+++ sources/src/__generated__/ztinywave.cache.ts
@@ -87,5 +87,137 @@ export const source = [
     "reserved2": 115,
     "reserved2Input": "uV6})Q'?D{SeAMX4;Ef",
     "reserved2Output": "91ysxe8luorn6vpciwq"
+  },
+  {
+    "id": "fkad",
+    "input": "7z>g}{/W#`c[ZT&sI<2-haKXYn\no)xSFA",
+    "output": "niue8tmyacj3l91q65fxbwzrv7po2gk40",
+    "reserved1": 115,
+    "reserved1Input": "1EHb.|u?p(qfPity'r6O%\tw=]9BJRD;:8k",
+    "reserved1Output": "phm21v9cw4b73yglnfxotazsuj8qrk650i",
+    "reserved2": 104,
+    "reserved2Input": "Qj^ NCUv54le\"VMm_30GL",
+    "reserved2Output": "bixqrhwn9zjm54o12fe0s"
+  },
+  {
+    "id": "svmm",
+    "input": "maG;FT.e2cYzV:%i})g4bp-KUZy<wL\n_?",
+    "output": "1nwsr7vk8fh0l3ut92jigqx6z45cpbmyo",
+    "reserved1": 97,
+    "reserved1Input": "krBJtD(Qlv^>#[h{X19o&SEAM/n\"O= 05P",
+    "reserved1Output": "36qz0mv9tne7kwra5i1ujx28oyfspcglh4",
+    "reserved2": 101,
+    "reserved2Input": "|`\t'Rqj6CNWH]I8sx7u3f",
+    "reserved2Output": "wegyjpzux38q2a9vmifot"
+  },
+  {
+    "id": "cokq",
+    "input": "(r%96jVK7{kP\tgDhOcFs'1;M\"IuQZ/yqA",
+    "output": "izxsomqh3p8bvga49w7fy2tluc6e5n01r",
+    "reserved1": 106,
+    "reserved1Input": "^zG[wm\nC`=8Bv><&]0:T L#px3Xli-n).4",
+    "reserved1Output": "p91tzx4ibshwf3qyen506ugo2mkv8lrjc7",
+    "reserved2": 107,
+    "reserved2Input": "EYf?RN2a}WbU5eH_|StJo",
+    "reserved2Output": "sm8fkhrwa94y0eupj2nq1"
+  },
+  {
+    "id": "znbg",
+    "input": "(\tEHC_;s/.WgNfVl z9MYhQ}Tj:JFUS)#",
+    "output": "o5hkm78up2yxwvzsj0at1bglr6ei43ncq",
+    "reserved1": 102,
+    "reserved1Input": "KZi{^4<m\"5'c%\nXLurypqA8[eIw-DR|kbB",
+    "reserved1Output": "ou7ercvw9l01yghnfip685bzxqtak3sjm4",
+    "reserved2": 57,
+    "reserved2Input": "=6a0G>POo37nv?x&1`]2t",
+    "reserved2Output": "crxmjf7yhgt6o3p8l09iv"
+  },
+  {
+    "id": "zmpc",
+    "input": ".Ip>szJ8EwahYM/v;yKc=f\n([_T q3nOS",
+    "output": "5nv1iegza269ju8soky04h3p7rltcxmbq",
+    "reserved1": 119,
+    "reserved1Input": "QCABV\"]#0jb&Wo6t:UuiHg1rPD4%)RlNk-",
+    "reserved1Output": "le4971m5irpukxjv3sq2o8y60gnbzthwfa",
+    "reserved2": 102,
+    "reserved2Input": "G752e`LXx'Zm}9<\t{?F",
+    "reserved2Output": "plbi21cxfmo36tn50uq"
+  },
+  {
+    "id": "fwbh",
+    "input": "x5A8hE9=\nQG1\"CuJ'oV(I)>lbW4D#e L6",
+    "output": "wr275yogsj4kv03izcnx1uaqb8pflm6th",
+    "reserved1": 57,
+    "reserved1Input": "3%t\t{2rckfyXR]?NK&/Fgw;TSMmn-7_iU.",
+    "reserved1Output": "y93rfts7lxq8vkgjen5m0i6wzhap1ucb2o",
+    "reserved2": 101,
+    "reserved2Input": "}qB<z`PZ[YH:0sjvaOp",
+    "reserved2Output": "6m7kwe3qaohu5g4bz8i"
+  },
+  {
+    "id": "qbuw",
+    "input": "i;Uj'[<w\"DT0lZLgS8f#hxsMV}ty:E-)`",
+    "output": "3c51goq40pzal9r6xuvtkey2swnj7mihb",
+    "reserved1": 102,
+    "reserved1Input": "k96YK(v/&ICmGB\t5bza?HcP_1W qRoA.=\n",
+    "reserved1Output": "mteqh0ys62p1fxj589ro4aunzlig3c7wbk",
+    "reserved2": 56,
+    "reserved2Input": "{eF%OXrQ2>np3JN]47u",
+    "reserved2Output": "9jmanwyqx0s54zuto6h"
+  },
+  {
+    "id": "nloc",
+    "input": "x<[ykY%1-sK9_C0Raj#8OLl]/HwhqFU\t3",
+    "output": "0ozupkrx6qjwnygl34m7i9th1f8v2bec5",
+    "reserved1": 97,
+    "reserved1Input": "&m2GT5IrP\" .B(o=:igbJpWnz7tcvNZ`>\n",
+    "reserved1Output": "74hfvbtcj2eroa0ul56yw3inqxkgp9m1zs",
+    "reserved2": 115,
+    "reserved2Input": "uV6})Q'?D{SeAMX4;Ef",
+    "reserved2Output": "91ysxe8luorn6vpciwq"
+  },
+  {
+    "id": "kynb",
+    "input": "0Nm}ubC9L6k{7(\"nx>s=Ko]IBw/ySgf%W",
+    "output": "a274bcljtv0omw6z9g5p13us8ekhxirfq",
+    "reserved1": 121,
+    "reserved1Input": "e?`3AU<2Z[;\tiJEFV'|Pa8T5j-.v :q#H)",
+    "reserved1Output": "eow5rfluq8x4zgj70p13ci6mh2s9tnkabv",
+    "reserved2": 110,
+    "reserved2Input": "&XD_QOhMrltpR1c4zG\nY",
+    "reserved2Output": "c7863qbsmwy54otnhiv1"
+  },
+  {
+    "id": "uyyk",
+    "input": "Tx<W|XvcubCz-ekU/ oiM\t&:%Ig>2h{s[",
+    "output": "mjb0feu6lzx7kqhgop4at831c92ywir5n",
+    "reserved1": 118,
+    "reserved1Input": "5f;Vt\"qDLB91'm\n}?R6S(Z#A`Jr0HQlKP)",
+    "reserved1Output": "qp5r842ycvxjowbaz7uhts9gk6i0emn13f",
+    "reserved2": 115,
+    "reserved2Input": "NGnE8a_y.FjYO=4w3]p7",
+    "reserved2Output": "kn1eho6fjbr40pxc9i2q"
+  },
+  {
+    "id": "rypa",
+    "input": "B8 /Y9o]VHC_p3yt\nhTONi5q6IG-r=2g%",
+    "output": "ul2ow01j9zq58mfk4cv37absyigteh6xn",
+    "reserved1": 112,
+    "reserved1Input": "nEeJKa.:P{A;x0UL\"4'XFm|}wfvzQ#7ZS(",
+    "reserved1Output": "m8961qwzghu27xeoapn3k5ilvrfjc4syt0",
+    "reserved2": 114,
+    "reserved2Input": "&j<RDs[W>bukM`c?1)\tl",
+    "reserved2Output": "6akyhor80m37slfw1vxp"
+  },
+  {
+    "id": "ehor",
+    "input": "\"q4\nJ3fkZhj&lt`wH50T='g|(EP[B-QYD",
+    "output": "7uil5a3gxbrptvjeymo4c09wqzs682nf1",
+    "reserved1": 107,
+    "reserved1Input": "<GcpOI/\tzAX%6?9o r.bmFRie>Ks_)v;Vu",
+    "reserved1Output": "ypwzx2usm8og5q74tanlb6ri3vcehj9kf1",
+    "reserved2": 104,
+    "reserved2Input": "7y:x218]U#NC}nWaMS{L",
+    "reserved2Output": "bja3zkfvqltgu5sw678n"
   }
 ];
