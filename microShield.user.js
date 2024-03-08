b65e5ffc167f7cf30cef3db8755c71d5f0175675
piquark6046 <piquark6046@proton.me>
2024-03-08 09:08:37 +0000

    chore: add meeco.kr
    https://github.com/AdguardTeam/AdguardFilters/issues/174399

--- microShield.user.js
+++ microShield.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://cdn.jsdelivr.net/gh/List-KR/microShield@latest/microShield.user.js
 // @license      Apache-2.0
 //
-// @version      3.16.4
+// @version      3.16.5
 // @author       PiQuark6046 and contributors
 //
 // @match        *://ygosu.com/*
@@ -97,6 +97,8 @@
 // @match        *://*.etnews.com/*
 // @match        *://iusm.co.kr/*
 // @match        *://*.iusm.co.kr/*
+// @match        *://meeco.kr/*
+// @match        *://*.meeco.kr/*
 //
 // @description        microShield allows AdGuard, uBlock Origin, Brave and ABP to resist against Ad-Shield widely.
 // @description:ko     microShield는 AdGuard, uBlock Origin, Brave 와 ABP가 애드쉴드에 널리 저항할 수 있도록 합니다.
