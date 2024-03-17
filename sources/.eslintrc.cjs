356db9bb916f935b606b3302c36bd8eb7445bfa5
piquark6046 <piquark6046@proton.me>
2024-03-17 09:03:42 +0000

    build: update ESLint

--- sources/.eslintrc.cjs
+++ sources/.eslintrc.cjs
@@ -10,6 +10,16 @@ module.exports = {
 	},
 	plugins: ['@typescript-eslint'],
 	rules: {
+		'@typescript-eslint/naming-convention': ['error', {
+			selector: ['variableLike', 'parameterProperty', 'classProperty', 'typeProperty'],
+			format: ['PascalCase']
+		}],
+		'@typescript-eslint/prefer-nullish-coalescing': 'off',
+		'new-cap': 'off',
+    'no-var': 'off',
+		'comma-dangle': 'off',
+		indent: ['off', 'tab'],
+		semi: ['error', 'never'],
 		'@typescript-eslint/no-unused-vars': 'off'
 	},
 	ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs']
