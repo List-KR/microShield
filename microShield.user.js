0891b8c6933fd6ca64f5d186f9b7cff486e0d43c
piquark6046 <j.w.so@adguard.com>
2024-03-22 11:18:41 +0000

    build userscript

--- microShield.user.js
+++ microShield.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://cdn.jsdelivr.net/gh/List-KR/microShield@latest/microShield.user.js
 // @license      Apache-2.0
 //
-// @version      3.19.4
+// @version      3.19.5
 // @author       PiQuark6046 and contributors
 //
 // @match        *://ygosu.com/*
@@ -107,6 +107,14 @@
 // @match        *://*.apkmirror.com/*
 // @match        *://uttranews.com/*
 // @match        *://*.uttranews.com/*
+// @match        *://fntimes.com/
+// @match        *://*.fntimes.com/
+// @match        *://javatpoint.com/*
+// @match        *://*.javatpoint.com/*
+// @match        *://text-compare.com/*
+// @match        *://*.text-compare.com/*
+// @match        *://vipotv.com/*
+// @match        *://*.vipotv.com/*
 //
 // @description        microShield allows AdGuard, uBlock Origin, Brave and ABP to resist against Ad-Shield widely.
 // @description:ko     microShield는 AdGuard, uBlock Origin, Brave 와 ABP가 애드쉴드에 널리 저항할 수 있도록 합니다.
