672d4754da79b04f1af119624cd8e906cd9b05d3
PiQuark6046 <j.w.so@adguard.com>
2024-04-15 12:37:26 +0000

    chore: add allthekingz.com (#140)
    * chore: add allthekingz.com
    
    https://github.com/AdguardTeam/AdguardFilters/issues/177052
    
    * chore: update version
    
    * build userscript

--- microShield.user.js
+++ microShield.user.js
@@ -8,7 +8,7 @@
 // @downloadURL  https://cdn.jsdelivr.net/gh/List-KR/microShield@latest/microShield.user.js
 // @license      Apache-2.0
 //
-// @version      4.2.10
+// @version      4.2.11
 // @author       PiQuark6046 and contributors
 //
 // @match        *://ygosu.com/*
@@ -119,6 +119,8 @@
 // @match        *://*.dt.co.kr/*
 // @match        *://g-enews.*/*
 // @match        *://*.g-enews.*/*
+// @match        *://allthekingz.com/*
+// @match        *://*.allthekingz.com/*
 //
 // @description        microShield allows AdGuard, uBlock Origin, Brave and ABP to resist against Ad-Shield widely.
 // @description:ko     microShield는 AdGuard, uBlock Origin, Brave 와 ABP가 애드쉴드에 널리 저항할 수 있도록 합니다.
