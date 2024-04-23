ab49deb1b754e975ac54a1b65b25693c174a1689
piquark6046 <j.w.so@adguard.com>
2024-04-23 10:05:27 +0000

    chore: add f6176563.site as Ad-Shield domain

--- sources/src/adshield/validators.ts
+++ sources/src/adshield/validators.ts
@@ -33,7 +33,8 @@ export const AdshieldDomains = [
 	'48a16802.site',
 	'7e809ed7-e553-4e29-acb1-4e3c0e986562.site',
 	'd84bc26d.site',
-	'b1181fb1.site'
+	'b1181fb1.site',
+	'f6176563.site'
 ]
 
 export const AdshieldKeywords = [
